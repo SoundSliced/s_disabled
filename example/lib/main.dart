3de7eab3ef8ce74b4a44212f0305fe6ef5ed15b7
Christophe Chanteur <soundsliced@gmail.com>
2025-12-11 23:20:21 +0000

    Release version 1.0.0
    - Updated version, documentation, and configuration
    - Organized scripts and added .gitattributes/.pubignore

---
+++ example/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:s_disabled/s_disabled.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ test/s_disabled_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:s_disabled/s_disabled.dart';
+
+void main() {
+
+}
